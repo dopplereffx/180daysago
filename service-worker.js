09032b06a5791dac1fc7747851aa6224b9c881fa
David Rose <doppler@gmail.com>
2019-03-26 09:22:01 -0400

    use relative paths in service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ const CACHE = "180-v3";
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE).then(function(cache) {
-      return cache.addAll(["/", "/index.html", "/logo-32x32.png"]);
+      return cache.addAll(["./", "./index.html", "./logo-32x32.png"]);
     })
   );
 });
